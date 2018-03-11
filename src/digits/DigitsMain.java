e2db50480636342cfd65b118ffc947bb8f1c5084
Douglas Urner <dlu@canishe.com>
2018-03-10 23:32:23 -0800

    Fix warnings from IntelliJ.

--- src/digits/DigitsMain.java
+++ src/digits/DigitsMain.java
@@ -6,7 +6,7 @@ public class DigitsMain {
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
     while (true) {
-      System.out.printf("Enter Digits: ");
+      System.out.print("Enter Digits: ");
       String str = stdin.next();
       if (str.equalsIgnoreCase("quit")) break;
       System.out.printf("'%s' %s strictly increasing\n", str,
